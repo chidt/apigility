b581a2ae852f5babb63c7adf96addc15b3015047
CHI-DT <chidt@unifiedx.vn>
2018-12-25 17:26:56 +0700

    Init Project

---
+++ module/Status/config/module.config.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    'service_manager' => [
+        'factories' => [],
+    ],
+    'router' => [
+        'routes' => [],
+    ],
+    'zf-versioning' => [
+        'uri' => [],
+    ],
+    'zf-rest' => [],
+    'zf-content-negotiation' => [
+        'controllers' => [],
+        'accept_whitelist' => [],
+        'content_type_whitelist' => [],
+    ],
+    'zf-hal' => [
+        'metadata_map' => [],
+    ],
+];
